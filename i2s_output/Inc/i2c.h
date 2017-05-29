5a583314580c9c66cc610b5b8a4ce132da5cafd8
Tarkan Dalay <tarkandalay52@gmail.com>
2017-05-29 04:57:27 +0300

    Initalized

---
+++ i2s_output/Inc/i2c.h
@@ -0,0 +1,193 @@
+/**
+  ******************************************************************************
+  * File Name          : I2C.h
+  * Description        : This file provides code for the configuration
+  *                      of the I2C instances.
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2017 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __i2c_H
+#define __i2c_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include "main.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+extern I2C_HandleTypeDef hi2c1;
+
+/* USER CODE BEGIN Private defines */
+
+#define CS_I2C_SPI_Pin GPIO_PIN_3
+#define CS_I2C_SPI_GPIO_Port GPIOE
+#define PC14_OSC32_IN_Pin GPIO_PIN_14
+#define PC14_OSC32_IN_GPIO_Port GPIOC
+#define PC15_OSC32_OUT_Pin GPIO_PIN_15
+#define PC15_OSC32_OUT_GPIO_Port GPIOC
+#define PH0_OSC_IN_Pin GPIO_PIN_0
+#define PH0_OSC_IN_GPIO_Port GPIOH
+#define PH1_OSC_OUT_Pin GPIO_PIN_1
+#define PH1_OSC_OUT_GPIO_Port GPIOH
+#define OTG_FS_PowerSwitchOn_Pin GPIO_PIN_0
+#define OTG_FS_PowerSwitchOn_GPIO_Port GPIOC
+#define PDM_OUT_Pin GPIO_PIN_3
+#define PDM_OUT_GPIO_Port GPIOC
+#define B1_Pin GPIO_PIN_0
+#define B1_GPIO_Port GPIOA
+#define I2S3_WS_Pin GPIO_PIN_4
+#define I2S3_WS_GPIO_Port GPIOA
+#define SPI1_SCK_Pin GPIO_PIN_5
+#define SPI1_SCK_GPIO_Port GPIOA
+#define SPI1_MISO_Pin GPIO_PIN_6
+#define SPI1_MISO_GPIO_Port GPIOA
+#define SPI1_MOSI_Pin GPIO_PIN_7
+#define SPI1_MOSI_GPIO_Port GPIOA
+#define BOOT1_Pin GPIO_PIN_2
+#define BOOT1_GPIO_Port GPIOB
+#define CLK_IN_Pin GPIO_PIN_10
+#define CLK_IN_GPIO_Port GPIOB
+#define LD4_Pin GPIO_PIN_12
+#define LD4_GPIO_Port GPIOD
+#define LD3_Pin GPIO_PIN_13
+#define LD3_GPIO_Port GPIOD
+#define LD5_Pin GPIO_PIN_14
+#define LD5_GPIO_Port GPIOD
+#define LD6_Pin GPIO_PIN_15
+#define LD6_GPIO_Port GPIOD
+#define I2S3_MCK_Pin GPIO_PIN_7
+#define I2S3_MCK_GPIO_Port GPIOC
+#define VBUS_FS_Pin GPIO_PIN_9
+#define VBUS_FS_GPIO_Port GPIOA
+#define OTG_FS_ID_Pin GPIO_PIN_10
+#define OTG_FS_ID_GPIO_Port GPIOA
+#define OTG_FS_DM_Pin GPIO_PIN_11
+#define OTG_FS_DM_GPIO_Port GPIOA
+#define OTG_FS_DP_Pin GPIO_PIN_12
+#define OTG_FS_DP_GPIO_Port GPIOA
+#define SWDIO_Pin GPIO_PIN_13
+#define SWDIO_GPIO_Port GPIOA
+#define SWCLK_Pin GPIO_PIN_14
+#define SWCLK_GPIO_Port GPIOA
+#define I2S3_SCK_Pin GPIO_PIN_10
+#define I2S3_SCK_GPIO_Port GPIOC
+#define I2S3_SD_Pin GPIO_PIN_12
+#define I2S3_SD_GPIO_Port GPIOC
+#define Audio_RST_Pin GPIO_PIN_4
+#define Audio_RST_GPIO_Port GPIOD
+#define OTG_FS_OverCurrent_Pin GPIO_PIN_5
+#define OTG_FS_OverCurrent_GPIO_Port GPIOD
+#define SWO_Pin GPIO_PIN_3
+#define SWO_GPIO_Port GPIOB
+#define Audio_SCL_Pin GPIO_PIN_6
+#define Audio_SCL_GPIO_Port GPIOB
+#define Audio_SDA_Pin GPIO_PIN_9
+#define Audio_SDA_GPIO_Port GPIOB
+#define MEMS_INT2_Pin GPIO_PIN_1
+#define MEMS_INT2_GPIO_Port GPIOE
+
+/* USER CODE BEGIN Private defines */
+
+#define CODEC_I2C_ADDRESS 0x94
+#define CODEC_MAPBYTE_INC 0x80
+#define CODEC_I2C I2C1
+
+//register map bytes for CS42L22 (see page 35)
+#define CODEC_MAP_CHIP_ID 0x01
+#define CODEC_MAP_PWR_CTRL1 0x02
+#define CODEC_MAP_PWR_CTRL2 0x04
+#define CODEC_MAP_CLK_CTRL  0x05
+#define CODEC_MAP_IF_CTRL1  0x06
+#define CODEC_MAP_IF_CTRL2  0x07
+#define CODEC_MAP_PASSTHROUGH_A_SELECT 0x08
+#define CODEC_MAP_PASSTHROUGH_B_SELECT 0x09
+#define CODEC_MAP_ANALOG_SET 0x0A
+#define CODEC_MAP_PASSTHROUGH_GANG_CTRL 0x0C
+#define CODEC_MAP_PLAYBACK_CTRL1 0x0D
+#define CODEC_MAP_MISC_CTRL 0x0E
+#define CODEC_MAP_PLAYBACK_CTRL2 0x0F
+#define CODEC_MAP_PASSTHROUGH_A_VOL 0x14
+#define CODEC_MAP_PASSTHROUGH_B_VOL 0x15
+#define CODEC_MAP_PCMA_VOL 0x1A
+#define CODEC_MAP_PCMB_VOL 0x1B
+#define CODEC_MAP_BEEP_FREQ_ONTIME 0x1C
+#define CODEC_MAP_BEEP_VOL_OFFTIME 0x1D
+#define CODEC_MAP_BEEP_TONE_CFG 0x1E
+#define CODEC_MAP_TONE_CTRL 0x1F
+#define CODEC_MAP_MASTER_A_VOL 0x20
+#define CODEC_MAP_MASTER_B_VOL 0x21
+#define CODEC_MAP_HP_A_VOL 0x22
+#define CODEC_MAP_HP_B_VOL 0x23
+#define CODEC_MAP_SPEAK_A_VOL 0x24
+#define CODEC_MAP_SPEAK_B_VOL 0x25
+#define CODEC_MAP_CH_MIX_SWAP 0x26
+#define CODEC_MAP_LIMIT_CTRL1 0x27
+#define CODEC_MAP_LIMIT_CTRL2 0x28
+#define CODEC_MAP_LIMIT_ATTACK 0x29
+#define CODEC_MAP_OVFL_CLK_STATUS 0x2E
+#define CODEC_MAP_BATT_COMP 0x2F
+#define CODEC_MAP_VP_BATT_LEVEL 0x30
+#define CODEC_MAP_SPEAK_STATUS 0x31
+#define CODEC_MAP_CHARGE_PUMP_FREQ 0x34
+/* USER CODE END Private defines */
+
+extern void _Error_Handler(char *, int);
+
+void MX_I2C1_Init(void);
+
+/* USER CODE BEGIN Prototypes */
+void dac_control_i2c_init(void);
+void dac_control_i2c_send_data(uint8_t controlBytes[], uint8_t numBytes);
+uint8_t read_codec_register(uint8_t mapbyte);
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__ i2c_H */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
